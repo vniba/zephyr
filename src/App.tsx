81af2216abe11d31c55b18a382b039c85dbed273
vn!ba <itsmescorona@gmail.com>
2023-07-13 12:09:49 +0530

    feat: add routing to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles.ts';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard.tsx';
+import Bookings from './pages/Bookings.tsx';
+import Cabins from './pages/Cabins.tsx';
+import Login from './pages/Login.tsx';
+import Settings from './pages/Settings.tsx';
+import Users from './pages/Users.tsx';
+import PageNotFound from './pages/PageNotFound.tsx';
+import Account from './pages/Account.tsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' />
-        <Route path='/dashboard' element={false} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to='dashboard' />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='bookings' element={<Bookings />} />
+          <Route path='cabins' element={<Cabins />} />
+          <Route path='login' element={<Login />} />
+          <Route path='account' element={<Account />} />
+          <Route path='settings' element={<Settings />} />
+          <Route path='users' element={<Users />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import { useMoveBack } from "src/hooks/useMoveBack.ts";
-import Heading from "../ui/Heading";
+import { useMoveBack } from '../hooks/useMoveBack.ts';
+import Heading from '../ui/Heading';
 
 const StyledPageNotFound = styled.main`
   height: 100vh;
