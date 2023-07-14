78973775dc1f780ac20a86b07696a0c554159464
vn!ba <itsmescorona@gmail.com>
2023-07-14 13:31:21 +0530

    test: Button component

--- src/ui/Button.test.tsx
+++ src/ui/Button.test.tsx
@@ -7,6 +7,21 @@ describe('Button component', function () {
     render(<Button>Click</Button>);
     expect(screen.getByRole('button', { name: /click/i })).toBeInTheDocument();
   });
+  it('should render correct style => danger && large', function () {
+    const { container } = render(
+      <Button variation='danger' size='large'>
+        hello
+      </Button>,
+    );
+    expect(container.firstChild).toHaveStyleRule('font-size', '1.6rem');
+  });
+  it('should render correct style => secondary', function () {
+    const { container } = render(<Button variation='secondary'>hello</Button>);
+    expect(container.firstChild).toHaveStyleRule(
+      'color',
+      'var(--color-grey-600)',
+    );
+  });
 
   it('should matches default button snapshot', function () {
     const { container } = render(<Button>test</Button>);
